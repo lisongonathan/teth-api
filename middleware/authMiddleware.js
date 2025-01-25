808bfbb74d6de664ba33288888831b2d5ce2f35f
lisongonathan <lisongobaita@gmail.com>
2025-01-25 11:40:47 +0100

    Add middleware to secure endpoint /user

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ status: 401, message: 'Token manquant' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ status: 401, message: 'Token invalide', error });
+  }
+};
+
+module.exports = authMiddleware;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,13 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const authMiddleware = require('../middleware/authMiddleware');
+
 const userController = new UserController();
 
-// Route pour récupérer tous les utilisateurs
+// Protéger les routes avec le middleware d'authentification
+router.use(authMiddleware);
+
 router.get('/cagnote', (req, res) => userController.cagnote(req, res));
 router.get('/parties', (req, res) => userController.parties(req, res));
 router.get('/users', (req, res) => userController.users(req, res));
