8e5cf81c39c290a957a9262e9392f81454d69a64
unknown <lisongobaita@gmail.com>
2025-01-31 23:07:29 -0800

    Debuging router apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,13 +3,15 @@
 const express = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/AuthController');
+const AppController = require('../controllers/AppController');
 
 const authController = new AuthController()
+const appController = new AppController()
 // Route pour récupérer tous les utilisateurs
 router.post('/', (req, res) => authController.signin(req, res));
 router.post('/register', (req, res) => authController.signup(req, res));
 router.post('/recovery', (req, res) => authController.recovery(req, res));
-router.get('/jetons', (req, res) => authController.allTokens(req, res));
-router.post('/buyToken', (req, res) => authController.buyToken(req, res));
+router.get('/jetons', (req, res) => appController.allTokens(req, res));
+router.post('/buyToken', (req, res) => appController.buyToken(req, res));
 
 module.exports = router;
